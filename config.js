c077f384b7e6005d2d461807d77f690174ac0eb2
Aaron52077 <Aaron52077@users.noreply.github.com>
2021-06-26 01:51:07 +0000

    Automated deployment: Sat Jun 26 01:51:07 UTC 2021 6be16e6214eaea7459eaf5ea0d4afea0c07dba18

---
+++ config.js
@@ -0,0 +1,6 @@
+window.appConfig = {
+  // 后端接口地址
+  APP_SERVER_API: "https://www.mock-api.com",
+  VERSION: "1.1.0",
+  TIMESTAMP: new Date().getTime()
+};
---
+++ precache-manifest.fa189277fcc89d0093d00e0922a1c723.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f52a4d2afeb24f949a858f6dfe48a67",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d49e93ba3e720cfaaa19",
+    "url": "./static/css/2.2aa9ecfd.chunk.css"
+  },
+  {
+    "revision": "fbcb6e189f7b516a7a7f",
+    "url": "./static/css/main.b82c4105.chunk.css"
+  },
+  {
+    "revision": "d49e93ba3e720cfaaa19",
+    "url": "./static/js/2.9d214a51.chunk.js"
+  },
+  {
+    "revision": "9cb8b8ad88d4e2ffdf4b778625f94edd",
+    "url": "./static/js/2.9d214a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d34529180a495a099b7b",
+    "url": "./static/js/3.c58ab691.chunk.js"
+  },
+  {
+    "revision": "95dcf5356e8c199a9a13",
+    "url": "./static/js/4.3acb1dc0.chunk.js"
+  },
+  {
+    "revision": "fbcb6e189f7b516a7a7f",
+    "url": "./static/js/main.397671e8.chunk.js"
+  },
+  {
+    "revision": "dd35196760d121eff7ad",
+    "url": "./static/js/runtime-main.4338d000.js"
+  }
+]);
