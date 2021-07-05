c9aeefc16fe56536a3ae47e25fb569882fd864dd
Aaron <chao_code520@163.com>
2021-07-05 10:47:25 +0800

    no message

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import history from './history';
-import AccountIn from '@/pages/account';
-import AccountOAuthIn from '@/pages/account/oauth';
-import BaseLayout from '@/layouts';
-import AxureScreen from '@/pages/axure-editor';
-import FormRender from '@/pages/dashboard/formRender';
-import HtmlSpreadsheet from '@/pages/spreadsheet';
-import AxureScreenPanel from '@/pages/axure-editor/preview';
-import { pathToParam } from '~utils';
+import React, { Component } from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import history from "./history";
+import AccountIn from "@/pages/account";
+import AccountOAuthIn from "@/pages/account/oauth";
+import BaseLayout from "@/layouts";
+import AxureScreen from "@/pages/designer";
+import FormRender from "@/pages/dashboard/formRender";
+import HtmlSpreadsheet from "@/pages/spreadsheet";
+import AxureScreenPanel from "@/pages/designer/preview";
+import { pathToParam } from "~utils";
 
 class CreateRouter extends Component {
   render() {
@@ -30,8 +30,8 @@ class CreateRouter extends Component {
               const { token } = pathToParam();
               if (token) {
                 // TODO：优先清空存储的token
-                dispatch({ type: 'app/resetState' });
-                return <Redirect to={'/oauth?token=' + token} />;
+                dispatch({ type: "app/resetState" });
+                return <Redirect to={"/oauth?token=" + token} />;
               }
               return !accessToken ? <Redirect from="/" to="/account" /> : <BaseLayout />;
             }}
